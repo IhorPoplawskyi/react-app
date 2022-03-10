4f203f32618d445cee1432af83ea5077428dad69
Ihor Poplavskyi <poplawskyiihor@gmail.com>
2022-03-10 17:17:52 +0200

    lesson 37 store state oop

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,58 +1,65 @@
-import { rerenderEntireTree } from "../../render";
-
-const state = {
-  profilePage: {
-    posts: [
-      { id: 1, text: "Hello world" },
-      { id: 2, text: "Hello JavaScript" },
-      { id: 3, text: "Im learning React" },
-    ],
-    newPostText: '',
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, text: "Hello world" },
+        { id: 2, text: "Hello JavaScript" },
+        { id: 3, text: "Im learning React" },
+      ],
+      newPostText: '',
+    },
+    messagesPage: {
+      dialogs: [
+        { id: 1, name: "Ihor" },
+        { id: 2, name: "Vova" },
+        { id: 3, name: "Andriy" },
+        { id: 4, name: "Anatoliy" },
+        { id: 5, name: "Oksana" },
+        { id: 6, name: "Nazar" },
+      ],
+      messages: [
+        { id: 1, message: "Hello world" },
+        { id: 2, message: "Hello JavaScript" },
+        { id: 3, message: "Aloha" },
+        { id: 4, message: "Nihao" },
+        { id: 5, message: "Namaste" },
+        { id: 6, message: "Pryvit!" },
+      ],
+    },
+    sitebar: {
+      friends: [
+        { id: 1, name: "Ihor" },
+        { id: 2, name: "Andriy" },
+        { id: 3, name: "Aloha" },
+      ]
+    }
   },
-  messagesPage: {
-    dialogs: [
-      { id: 1, name: "Ihor" },
-      { id: 2, name: "Vova" },
-      { id: 3, name: "Andriy" },
-      { id: 4, name: "Anatoliy" },
-      { id: 5, name: "Oksana" },
-      { id: 6, name: "Nazar" },
-    ],
-    messages: [
-      { id: 1, message: "Hello world" },
-      { id: 2, message: "Hello JavaScript" },
-      { id: 3, message: "Aloha" },
-      { id: 4, message: "Nihao" },
-      { id: 5, message: "Namaste" },
-      { id: 6, message: "Pryvit!" },
-    ],
+  getState() {
+    return this._state;
   },
-  sitebar: {
-    friends: [
-      { id: 1, name: "Ihor" },
-      { id: 2, name: "Andriy" },
-      { id: 3, name: "Aloha" },
-    ]
+  _callSubscriber() {
+    console.log('state chaged')
+  },
+  addPost() {
+    let newPost = {
+      id: `${Math.random()}`,
+      text: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+  
+    this._state.profilePage.posts.push(newPost);
+    this.updateNewPostText('');
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe (observer) {
+    this._callSubscriber = observer;
   }
-};
-
-export const addPost = () => {
-  let newPost = {
-    id: `${Math.random()}`,
-    text: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-
-  state.profilePage.posts.push(newPost);
-  updateNewPostText('');
-  rerenderEntireTree(state);
 }
 
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
+window.state = store._state;
 
-window.state = state;
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from './../src/components/redux/state';
-import {addPost} from './../src/components/redux/state';
-import { rerenderEntireTree } from './render';
+import store from './../src/components/redux/state';
 
-rerenderEntireTree(state);
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
